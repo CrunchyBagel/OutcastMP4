54053b901164e044c899b12bf996257272d1fc2a
Quentin Zervaas <quentin@crunchybagel.com>
2018-12-04 22:42:18 +1030

    first commit

---
+++ OutcastMP4/MP4File.swift
@@ -0,0 +1,13 @@
+//
+//  MP4File.swift
+//  OutcastMP4
+//
+//  Created by Quentin Zervaas on 4/12/18.
+//  Copyright © 2018 Crunchy Bagel. All rights reserved.
+//
+
+import Cocoa
+
+class MP4File: NSObject {
+
+}
